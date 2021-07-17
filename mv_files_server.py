f7a997fea952f7448ffaf2113cba743848fb1e29
jaysmrit <jayasmritha.s@gmail.com>
2021-07-17 19:42:22 +0530

    Update mv_files_server.py

--- mv_files_server.py
+++ mv_files_server.py
@@ -1,10 +1,12 @@
 import paramiko
-ssh = paramiko.SSHClient()
-ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-ssh.connect(hostname ='host_name', user_name ='username',password ='pwd',port = 22)
-sftp_client = ssh.open_sftp()
+def cred_path(host_name,username,pwd,read_path,write_path):
+  ssh = paramiko.SSHClient()
+  ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+  #Provide the host name from which the file need to be read
+  ssh.connect(hostname ='host_name', user_name ='username',password ='pwd',port = 22)
+  sftp_client = ssh.open_sftp()
 
-sftp_client.get('read_path','write_path')
+  sftp_client.get('read_path','write_path')
 
-sftp_client.close()
-ssh.close()
+  sftp_client.close()
+  ssh.close()
